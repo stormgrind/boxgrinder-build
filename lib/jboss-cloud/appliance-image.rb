73ad24780e9cacca64a00b11dc036553a21aec89
Marek Goldmann <marek.goldmann@gmail.com>
2009-03-03 10:32:51 +0100

    first progressbard integration, not working right now

--- lib/jboss-cloud/appliance-image.rb
+++ lib/jboss-cloud/appliance-image.rb
@@ -1,7 +1,9 @@
 
 require 'rake/tasklib'
 require 'jboss-cloud/appliance-vmx-image'
+require 'progressbar/progressbar'
 require 'yaml'
+require 'thread'
 
 module JBossCloud
   class ApplianceImage < Rake::TaskLib
@@ -18,9 +20,9 @@ module JBossCloud
 
     def define
 
-      appliance_build_dir     = "#{@build_dir}/appliances/#{@config.arch}/#{@config.name}"
-      kickstart_file          = "#{appliance_build_dir}/#{@config.name}.ks"
-      xml_file                = "#{appliance_build_dir}/#{@config.name}.xml"
+      @appliance_build_dir     = "#{@build_dir}/appliances/#{@config.arch}/#{@config.name}"
+      kickstart_file          = "#{@appliance_build_dir}/#{@config.name}.ks"
+      xml_file                = "#{@appliance_build_dir}/#{@config.name}.xml"
       super_simple_name       = File.basename( @config.name, '-appliance' )
 
       desc "Build #{super_simple_name} appliance."
@@ -36,12 +38,35 @@ module JBossCloud
       file xml_file => [ kickstart_file, "appliance:#{@config.name}:rpms", tmp_dir ] do
         Rake::Task[ 'rpm:repodata:force' ].invoke
 
+        show_progressbar
+
         command = "sudo PYTHONUNBUFFERED=1 appliance-creator -d -v -t #{tmp_dir} --cache=#{@rpms_cache_dir}/#{@config.arch} --config #{kickstart_file} -o #{@build_dir}/appliances/#{@config.arch} --name #{@config.name} --vmem #{@config.mem_size} --vcpu #{@config.vcpu}"
         execute_command( command )
+        
       end
 
       ApplianceVMXImage.new( @config )
 
+    end
+
+    def show_progressbar
+      t = Thread.new do
+        disk_file = "#{@appliance_build_dir}/#{@config.name}-sda.raw"
+        sleep_time = 0.05
+        
+        #while !File.exists?( disk_file )
+        #  sleep( 1 )
+        #end
+
+        pbar = ProgressBar.new("#{@config.name}-sda.raw", @config.disk_size)
+
+        200.times {
+          sleep( sleep_time )
+          pbar.inc #( File.size( disk_file ) / ( 1024 * 1024) )
+        }
+
+        pbar.finish
+      end
     end
   end
 end
